9244771d8e7d8d790814e9f01855d6fb779feb16
Abdurrahman Ziyad <abdurrahman.ziyad17@gmail.com>
2023-06-28 05:36:59 +0700

    Installasi dan Konfigurasi Package Pendukung

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+//import Toaster
+import { Toaster } from 'react-hot-toast';
+
+function App() {
+
+  return (
+    <>
+      <Toaster />
+      <h1>Tes</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+//import BrowserRouter dari react router
+import { BrowserRouter } from 'react-router-dom';
+
+//import CSS Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//mapbox gl CSS
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+//mapbox gl directions CSS
+import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
+
+//mapbox gl geocoder CSS
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+  </React.StrictMode>,
+)
