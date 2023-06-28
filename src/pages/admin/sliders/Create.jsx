38d5166776e1cd24a75e32cc3327bfd42c8fbc8b
Abdurrahman Ziyad <abdurrahman.ziyad17@gmail.com>
2023-06-28 15:54:37 +0700

    Membuat Proses Create Data Slider

--- src/pages/admin/sliders/Create.jsx
+++ src/pages/admin/sliders/Create.jsx
@@ -1,12 +1,113 @@
-//import react  
-import React from "react";
+//import hook useState from react
+import React, { useState } from "react";
 
-//import layout admin
+//import layout
 import LayoutAdmin from "../../../layouts/Admin";
+
+//import BASE URL API
+import Api from "../../../api";
+
+//import hook navigate dari react router dom
+import { useNavigate } from "react-router-dom";
+
+//import js cookie
+import Cookies from "js-cookie";
+
+//import toats
+import toast from "react-hot-toast";
 
 function SliderCreate() {
 
-    return(
+    //title page
+    document.title = "Add New Slider - Administrator Travel GIS";
+
+    //state
+    const [image, setImage] = useState("");
+
+    //state validation
+    const [validation, setValidation] = useState({});
+
+    //token
+    const token = Cookies.get("token");
+
+    //navigate
+    const navigate = useNavigate();
+
+    //function "handleFileChange"
+    const handleFileChange = (e) => {
+
+        //define variable for get value image data
+        const imageData = e.target.files[0]
+
+        //check validation file
+        if (!imageData.type.match('image.*')) {
+
+            //set state "image" to null
+            setImage('');
+
+            //show toast
+            toast.error("Format File not Supported!", {
+                duration: 4000,
+                position: "top-right",
+                style: {
+                    borderRadius: '10px',
+                    background: '#333',
+                    color: '#fff',
+                },
+            });
+
+            return
+        }
+
+        //assign file to state "image"
+        setImage(imageData);
+    }
+
+    //function "storeSlider"
+    const storeSlider = async (e) => {
+        e.preventDefault();
+
+        //define formData
+        const formData = new FormData();
+
+        //append data to "formData"
+        formData.append('image', image);
+
+        await Api.post('/api/admin/sliders', formData, {
+
+                //header
+                headers: {
+                    //header Bearer + Token
+                    'Authorization': `Bearer ${token}`,
+                    'content-type': 'multipart/form-data'
+                }
+
+            }).then(() => {
+
+                //show toast
+                toast.success("Data Saved Successfully!", {
+                    duration: 4000,
+                    position: "top-right",
+                    style: {
+                        borderRadius: '10px',
+                        background: '#333',
+                        color: '#fff',
+                    },
+                });
+
+                //redirect dashboard page
+                navigate("/admin/sliders");
+
+            })
+            .catch((error) => {
+
+                //set state "validation"
+                setValidation(error.response.data);
+            })
+
+    }
+
+    return (
         <React.Fragment>
             <LayoutAdmin>
                 <div className="row mt-4">
@@ -15,13 +116,29 @@ function SliderCreate() {
                             <div className="card-header">
                                 <span className="font-weight-bold"><i className="fa fa-images"></i> ADD NEW SLIDER</span>
                             </div>
+                            <div className="card-body">
+                                <form onSubmit={storeSlider}>
+                                    <div className="mb-3">
+                                        <label className="form-label fw-bold">Image</label>
+                                        <input type="file" className="form-control" onChange={handleFileChange}/>
+                                    </div>
+                                    {validation.image && (
+                                        <div className="alert alert-danger">
+                                            {validation.image[0]}
+                                        </div>
+                                    )}
+                                    <div>
+                                        <button type="submit" className="btn btn-md btn-success me-2"><i className="fa fa-save"></i> SAVE</button>
+                                        <button type="reset" className="btn btn-md btn-warning"><i className="fa fa-redo"></i> RESET</button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </div>
+              </div>
             </LayoutAdmin>
         </React.Fragment>
-    )
-
+    );
 }
 
-export default SliderCreate
+export default SliderCreate;
