0fb20d4c31bd0eb22ea93a7ebcb9b12d11153132
Abdurrahman Ziyad <abdurrahman.ziyad17@gmail.com>
2023-06-29 23:23:32 +0700

    Menampilkan Data Categories di Navbar

--- src/components/web/Header.jsx
+++ src/components/web/Header.jsx
@@ -1,5 +1,5 @@
 //import react and hook
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //import component react bootstrap
 import {
@@ -12,7 +12,34 @@ import {
 //import react router dom
 import { Link } from "react-router-dom";
 
+//import BASE URL API
+import Api from "../../api";
+
 function WebHeader() {
+
+    //state categories
+    const [categories, setCategories] = useState([]);
+
+    //function "fetchDataCategories"
+    const fetchDataCategories = async () => {
+
+        //fetching Rest API "categories"
+        await Api.get('/api/web/categories')
+            .then((response) => {
+
+                //set data to state
+                setCategories(response.data.data);
+            });
+    }
+
+    //hook
+    useEffect(() => {
+
+        //call function "fetchDataCategories"
+        fetchDataCategories();
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
         <React.Fragment>
@@ -23,6 +50,11 @@ function WebHeader() {
                     <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
                         <NavDropdown title={<span><i className="fa fa-list-ul"></i> CATEGORIES</span> } id="collasible-nav-dropdown" className="fw-bold text-white">
+                        {
+                            categories.map((category) => (
+                                <NavDropdown.Item as={Link} to={`/category/${category.slug}`} key={category.id}><img src={category.image} style={{ width: "35px" }} alt=""/> {category.name.toUpperCase()}</NavDropdown.Item>
+                            ))
+                        }
                         <NavDropdown.Divider />
                         <NavDropdown.Item as={Link} to="/posts/direction">LIHAT LAINNYA <i className="fa fa-long-arrow-alt-right"></i></NavDropdown.Item>
                         </NavDropdown>
