02eee2ff4091a5b9e91be5285e3ab7c2734987d9
Abdurrahman Ziyad <abdurrahman.ziyad17@gmail.com>
2023-06-29 23:24:56 +0700

    Menampilkan Data Sliders di Halaman Home

--- src/pages/web/home/Index.jsx
+++ src/pages/web/home/Index.jsx
@@ -4,10 +4,16 @@ import React from "react";
 //import layout web
 import LayoutWeb from "../../../layouts/Web";
 
+//import Slider component
+import Slider from '../../../components/web/Slider';
+
 function Home() {
   return (
     <React.Fragment>
       <LayoutWeb>
+      
+        <Slider />
+
         <div className="container mt-5">
             <h1>Home</h1>
         </div>
