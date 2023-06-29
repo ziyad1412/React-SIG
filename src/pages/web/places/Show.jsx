289699a8745403bc875f7b02b847f9212c22cf3d
Abdurrahman Ziyad <abdurrahman.ziyad17@gmail.com>
2023-06-29 23:51:07 +0700

    Menampilkan Halaman Detail Data Place

--- src/pages/web/places/Show.jsx
+++ src/pages/web/places/Show.jsx
@@ -1,21 +1,122 @@
-//import react
-import React from "react";
+//import hook react
+import React, { useEffect, useState, } from "react";
+
+//import react router dom
+import { Link, useParams } from "react-router-dom";
 
 //import layout web
 import LayoutWeb from "../../../layouts/Web";
 
-function WebPlacesShow() {
+//import BASE URL API
+import Api from "../../../api";
 
-    return(
+function WebPlaceShow() {
+
+    //state place
+    const [place, setPlace] = useState({});
+
+    //slug params
+    const { slug } = useParams();
+
+    //function "fetchDataPlace"
+    const fetchDataPlace = async () => {
+        //fetching Rest API
+        await Api.get(`/api/web/places/${slug}`)
+        .then((response) => {
+
+            //set data to state "places"
+            setPlace(response.data.data);
+
+            //set title from state "category"
+            document.title = `${response.data.data.title} - Website Wisata Berbasis GIS (Geographic Information System)`;
+        });
+    };
+
+    //hook
+    useEffect(() => {
+        //call function "fetchDataPlace"
+        fetchDataPlace();
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
         <React.Fragment>
-            <LayoutWeb>
-                <div className="container mt-80">
-                    Halaman Places Show
+          <LayoutWeb>
+            <div className="container mt-80">
+              <div className="row">
+                <div className="col-md-7 mb-4">
+                  <div className="card border-0 rounded shadow-sm">
+                    <div className="card-body">
+                      <h4>{place.title}</h4>
+                      <span className="card-text">
+                        <i className="fa fa-map-marker"></i> <i>{place.address}</i>
+                      </span>
+                      <hr />
+                      <div
+                        dangerouslySetInnerHTML={{ __html: place.description }}
+                      />
+                    </div>
+                  </div>
                 </div>
-            </LayoutWeb>
+                <div className="col-md-5 mb-4">
+                  <div className="card border-0 rounded shadow-sm">
+                    <div className="card-body">
+                      <h5>
+                        <i className="fa fa-map-marked-alt"></i> MAPS
+                      </h5>
+                      <hr />
+                    </div>
+                    <hr />
+                    <div className="card-body">
+                      <div className="row">
+                        <div className="col-md-2 col-2">
+                          <div className="icon-info-green">
+                            <i className="fa fa-map-marker-alt"></i>
+                          </div>
+                        </div>
+                        <div className="col-md-10 col-10">
+                            <div className="capt-info fw-bold">ADDRESS</div>
+                            <div className="sub-title-info"><i>{place.address}</i></div>
+                        </div>
+                        <div className="col-md-2 col-2">
+                          <div className="icon-info-green">
+                            <i className="fa fa-clock"></i>
+                          </div>
+                        </div>
+                        <div className="col-md-10 col-10">
+                          <div className="capt-info fw-bold">OFFICE HOURS</div>
+                          <div className="sub-title-info">{place.office_hours}</div>
+                        </div>
+                        <div className="col-md-2 col-2">
+                          <div className="icon-info-green">
+                            <i className="fa fa-phone"></i>
+                          </div>
+                        </div>
+                        <div className="col-md-10 col-10">
+                          <div className="capt-info fw-bold">PHONE</div>
+                          <div className="sub-title-info">{place.phone}</div>
+                        </div>
+                        <div className="col-md-2 col-2">
+                          <div className="icon-info-green">
+                            <i className="fa fa-globe-asia"></i>
+                          </div>
+                        </div>
+                        <div className="col-md-10 col-10">
+                          <div className="capt-info fw-bold">WEBSITE</div>
+                          <div className="sub-title-info">
+                            <a href={place.website} className="text-decoration-none">{place.website}</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </LayoutWeb>
         </React.Fragment>
-    )
-
+    );
 }
 
-export default WebPlacesShow;
+export default WebPlaceShow;
